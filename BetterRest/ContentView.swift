68fa6d00929b866f24067cb49570f313c0ce5b91
Issac Penn <pbbsignup@outlook.com>
2019-10-21 12:26:17 +0800

    Initial Commit

---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by PBB on 2019/10/21.
+//  Copyright © 2019 PBB. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
